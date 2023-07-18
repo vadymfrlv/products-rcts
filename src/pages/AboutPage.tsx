9280d2af761753243e8c854e9a071880cc52375c
Vadym Frolov <vdfroloff@gmail.com>
2023-07-18 15:17:44 +0300

    WORK:
    - AboutPage done

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas inventore perspiciatis
+      voluptatem, repudiandae autem placeat molestiae sit doloribus error, libero natus? Quia error
+      eius consequuntur magnam harum iste omnis facere voluptatem eligendi. Sunt nihil ipsa
+      doloribus earum! Ea recusandae ullam, odio unde velit laudantium ex ab quo. Maxime, sapiente
+      similique!
+    </p>
+  );
+};
+
+export default AboutPage;
