500c2ca46461d69ce92da341cf08265a71e98e64
Vadym Frolov <vdfroloff@gmail.com>
2023-07-18 15:07:39 +0300

    WORK:
    - routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import './style/global.css';
+import { ModalState } from './context/ModalContext.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ModalState>
+        <App />
+      </ModalState>
+    </BrowserRouter>
   </React.StrictMode>
 );
